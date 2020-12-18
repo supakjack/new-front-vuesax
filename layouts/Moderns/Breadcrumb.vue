20439e2a093ec1978253d1746a53001bbe287510
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-12-18 23:32:03 +0700

    add breadcrumb
    add breadcrumb

--- layouts/Moderns/Breadcrumb.vue
+++ layouts/Moderns/Breadcrumb.vue
@@ -3,11 +3,19 @@
     <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="11">
       <vs-card>
         <h3>
-          <vs-breadcrumb
-            align="left"
-            :items="breadcrumb"
-            separator="chevron_right"
-          ></vs-breadcrumb>
+          <vs-breadcrumb align="left" separator="chevron_right">
+            <ol class="breadcrumb">
+              <li
+                v-for="item in breadcrumb"
+                :key="item.title"
+                class="breadcrumb-item"
+              >
+                <nuxt-link :to="item.url" active-class="active">
+                  / {{ item.title }} 
+                </nuxt-link>
+              </li>
+            </ol>
+          </vs-breadcrumb>
         </h3>
       </vs-card>
     </vs-col>
@@ -22,4 +30,19 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="css" scoped>
+ol {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  text-decoration: none;
+}
+</style>
