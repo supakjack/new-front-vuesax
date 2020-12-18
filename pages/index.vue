a78af6b293631c9842a64aea261d348ea11b93ad
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2020-12-18 22:35:39 +0700

    debug
    debug

--- pages/index.vue
+++ pages/index.vue
@@ -1,157 +1,135 @@
 <template>
-  <div class="container">
+  <div class="flex flex-wrap">
     <div>
-      <header class="content-logos">
-        <logo />
-        <span class="plus">+</span>
-        <VuesaxLogo />
-      </header>
-      <h1 class="title">
-        Nuxt.js + Vuesax
-      </h1>
-      <h2 class="subtitle">
-        <a href="https://vuesax.com/">Vuesax</a> is a framework of ui components for <a href="https://vuejs.org/">Vuejs</a>, It was created to make new interfaces that have a new trend and are visually beautiful
-      </h2>
-      <div class="links">
-        <h3 class="h3">
-          Vuesax
-        </h3>
-        <a
-          href="https://vuesax.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://discordapp.com/invite/9dsKtvB"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          Discord
-        </a>
-        <a
-          href="https://github.com/lusaxweb/vuesax"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-      <div class="links">
-        <h3 class="h3">
-          Nuxt.js
-        </h3>
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <vs-row vs-justify="center">
+        <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+          <vs-card>
+            <div slot="header">
+              <h3>Hello world !</h3>
+            </div>
+            <div>
+              <span
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.</span
+              >
+            </div>
+            <div slot="footer">
+              <vs-row vs-justify="flex-end">
+                <vs-button
+                  type="gradient"
+                  color="danger"
+                  icon="favorite"
+                ></vs-button>
+                <vs-button color="primary" icon="turned_in_not"></vs-button>
+                <vs-button
+                  color="rgb(230,230,230)"
+                  color-text="rgb(50,50,50)"
+                  icon="settings"
+                ></vs-button>
+              </vs-row>
+            </div>
+          </vs-card>
+        </vs-col>
+      </vs-row>
+    </div>
+    <div>
+      <vs-row vs-justify="center">
+        <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+          <vs-card class="cardx">
+            <div slot="header">
+              <h3>Hello world !</h3>
+            </div>
+            <div slot="media">
+              <img src="https://lusaxweb.github.io/vuesax/card.png" />
+            </div>
+            <div>
+              <span
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.</span
+              >
+            </div>
+            <div slot="footer">
+              <vs-row vs-justify="flex-end">
+                <vs-button
+                  type="gradient"
+                  color="danger"
+                  icon="favorite"
+                ></vs-button>
+                <vs-button color="primary" icon="turned_in_not"></vs-button>
+                <vs-button
+                  color="rgb(230,230,230)"
+                  color-text="rgb(50,50,50)"
+                  icon="settings"
+                ></vs-button>
+              </vs-row>
+            </div>
+          </vs-card>
+        </vs-col>
+      </vs-row>
+    </div>
+    <div>
+      <vs-row vs-justify="center">
+        <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+          <vs-card actionable class="cardx">
+            <div slot="header">
+              <h3>Hello world !</h3>
+            </div>
+            <div slot="media">
+              <img src="https://lusaxweb.github.io/vuesax/card2.png" />
+            </div>
+            <div>
+              <span
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit</span
+              >
+            </div>
+            <div slot="footer">
+              <vs-row vs-justify="flex-end">
+                <vs-button color="primary" type="gradient">View</vs-button>
+                <vs-button color="danger" type="gradient">Delete</vs-button>
+              </vs-row>
+            </div>
+          </vs-card>
+        </vs-col>
+        <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
+          <vs-card actionable class="cardx">
+            <div slot="header">
+              <h3>Hello world !</h3>
+            </div>
+            <div slot="media">
+              <img src="https://lusaxweb.github.io/vuesax/card2.png" />
+            </div>
+            <div>
+              <span
+                >Lorem ipsum dolor sit amet, consectetur adipiscing elit</span
+              >
+            </div>
+            <div slot="footer">
+              <vs-row vs-justify="flex-end">
+                <vs-button color="primary" type="gradient">View</vs-button>
+                <vs-button color="danger" type="gradient">Delete</vs-button>
+              </vs-row>
+            </div>
+          </vs-card>
+        </vs-col>
+      </vs-row>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuesaxLogo from '~/components/VuesaxLogo.vue'
+import breadcrumb from '@/mixins/states/breadcrumb'
 
 export default {
-  components: {
-    Logo,
-    VuesaxLogo
+  layout: 'modern',
+  mixins: [breadcrumb],
+  mounted() {
+    console.log(this.breadcrumb)
   }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 55px;
-  color: #35495e;
-  letter-spacing: 1px;
-  text-transform: capitalize;
-  margin: 25px 0;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 1.1rem;
-  color: #526488;
-  word-spacing: 2px;
-  padding-bottom: 15px;
-  max-width: 600px;
-}
-
-.subtitle a {
-  font-weight: 500;
-  color: inherit;
-}
-
-.links {
-  padding-top: 15px;
-  margin-bottom: 20px;
-}
-
-.content-logos {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-width: 500px;
-}
-
-.plus {
-  font-size: 2.5rem;
-  margin: 15px;
-  color: #35495e;
-}
-
-.h3 {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-weight: 400;
-  margin: 10px;
-}
-</style>
+<style></style>
